f54ecee0c8476d5a1583452beb1b3a7edf842a8d
Mehdi Abaakouk <sileht@sileht.net>
2017-09-13 20:26:29 +0200

    Change logic of release script

--- release.sh
+++ release.sh
@@ -12,7 +12,9 @@ if [ "$1" ] ; then
     version="$1"
     if [ "$(get_version plugin.json)" != "$version" ]; then
         sed -i 's/"version": "[^"]*"/"version": "'$version'"/' plugin.json package.json
+        ./run-tests.sh
         git commit -m "Bump version $version" plugin.json package.json
+        git push
     fi
 else
     version=$(git tag | tail -1)
@@ -23,12 +25,13 @@ fi
 [ ! "$GITHUB_TOKEN" ] && error "GITHUB_TOKEN is missing"
 [ "$(get_version plugin.json)" != "$version" ] && error "plugin.json incorrect version"
 [ "$(get_version package.json)" != "$version" ] && error "package.json incorrect version"
-status=$(git status -sz)
-[ -z "$status" ] || error "Repo is not clean"
 
 echo "Building version $version"
 
 ./run-tests.sh
+
+status=$(git status -sz)
+[ -z "$status" ] || error "Repo is not clean after dist generation"
 
 echo
 echo "release: ${version} ? "
@@ -36,22 +39,18 @@ echo
 
 read
 
-git add -f dist/*
-git commit -m "Release version $version" dist/
-
 cp -a dist gnocchixyz-gnocchi-datasource
 tar -czf gnocchixyz-gnocchi-datasource-${version}.tar.gz gnocchixyz-gnocchi-datasource
 
 git tag $version -m "Release version $version"
-git push
 git push --tags
-
-nextversion=$(inc_version $version)
-sed -i 's/"version": "'$version'"/"version": "'$nextversion'"/' plugin.json package.json
-git commit -m "Bump version $nextversion" plugin.json package.json 
-git push
 
 github-release release -u gnocchixyz -r grafana-gnocchi-datasource --tag $version --description "Release $version for Grafana 3 and 4"
 github-release upload  -u gnocchixyz -r grafana-gnocchi-datasource --tag $version --name gnocchixyz-gnocchi-datasource-${version}.tar.gz --file gnocchixyz-gnocchi-datasource-${version}.tar.gz
+
+nextversion=$(inc_version $version)
+sed -i 's/"version": "'$version'"/"version": "'$nextversion'"/' plugin.json package.json
+git commit -m "Bump version $nextversion" plugin.json package.json
+git push
 
 rm -rf gnocchixyz-gnocchi-datasource*
